b15aff35bc412c8bf1d599d2e69a132c8bd046fe
Zamanov Nadir Rasim <Zamanov@itstep.edu.az>
2026-09-03 19:53:26 +0400

    Lesson 07

--- Django 07/Django_06_knowledge_hub_get_post/notes/data.py
+++ Django 07/Django_06_knowledge_hub_get_post/notes/data.py
@@ -1,6 +1,7 @@
 from copy import deepcopy
 from typing import Any
 
+from django.http import HttpRequest
 from unicodedata import category
 
 _NOTES:list[dict[str:Any]] = [
@@ -41,11 +42,53 @@ _NOTES:list[dict[str:Any]] = [
     }
 ]
 
+_next_id = 6
+
 def list_notes()-> list[dict[str, Any]]:
     return deepcopy(_NOTES)
+
 
 def get_note(note_id: int) -> dict[str, Any]|None:
     for note in _NOTES:
         if note['id'] == note_id:
             return deepcopy(note)
     return None
+
+
+def create_note(*, title: str, body: str, tag: str, category: str) -> dict[str, Any]:
+    global _next_id
+    note = {
+        'id': _next_id,
+        'title': title.strip(),
+        'body': body.strip(),
+        'tag': tag.strip(),
+        'category': category.strip()
+    }
+    _NOTES.append(note)
+    _next_id += 1
+    return deepcopy(note)
+
+
+def update_note(
+        note_id:int,
+        *,
+        title: str,
+        body: str,
+        tag: str,
+        category: str,
+) -> dict[str, Any]|None:
+    for note in _NOTES:
+        if note['id'] == note_id:
+            note['title'] = title.strip()
+            note['body'] = body.strip()
+            note['tag'] = tag.strip()
+            note['category'] = category.strip()
+            return deepcopy(note)
+    return None
+
+def delete_note(note_id: int) -> bool:
+    global _NOTES
+    before = len(_NOTES)
+    _NOTES = [n for n in _NOTES if n['id'] != note_id]
+    return before != len(_NOTES)
+
