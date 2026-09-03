17034ea2928afc254d15717b4aa77d7b13946f18
Zamanov Nadir Rasim <Zamanov@itstep.edu.az>
2026-09-03 20:27:47 +0400

    lesson 08 pre

--- Django 08/Django_08_knowledge_templates/Django_08_knowledge_templates/asgi.py
+++ Django 08/Django_08_knowledge_templates/Django_08_knowledge_templates/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Django_06_knowledge_templates project.
+ASGI config for Django_08_knowledge_templates project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Django_06_knowledge_templates.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Django_08_knowledge_templates.settings')
 
 application = get_asgi_application()
--- Django 08/Django_08_knowledge_templates/Django_08_knowledge_templates/wsgi.py
+++ Django 08/Django_08_knowledge_templates/Django_08_knowledge_templates/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Django_06_knowledge_templates project.
+WSGI config for Django_08_knowledge_templates project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Django_06_knowledge_templates.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Django_08_knowledge_templates.settings')
 
 application = get_wsgi_application()
